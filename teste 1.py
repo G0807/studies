ce440b7c1697af2b2c2e0dc52b47565f2decb2e0
Guto <miltonaugusto56@gmail.com>
2025-02-16 09:28:27 -0300

    Primeiro commit

---
+++ teste 1.py
@@ -0,0 +1,20 @@
+nome = input(' Qual é seu nome ?')
+idade = input('Qual é sua idade ?')
+peso = input('Qual é seu peso ?')
+print (nome, idade, peso)
+
+nome = input ('Qual seu nome ?')
+print ('Ola',nome,' prazer em te conhecer!')
+
+dia = input ('Dia = ')
+mes = input ('Mes =')
+ano = input ('Ano =')
+print (' Você nasceu no dia', dia, 'de' ,mes,'de' ,ano,'. Correto ?')
+
+numero = input (' Primeiro número ')
+num_dois = input ('Segundo número ')
+soma = int (numero) +int( num_dois)
+print ('A soma é', soma)
+print ('Boa noite')
+
+
---
+++ teste3.py
@@ -0,0 +1,3 @@
+salario_m = float(input('Digite o salário do mês: '))
+salario_anual = salario_m * 12
+print ('O salário anual é R${:.2f}'.format(salario_anual))
