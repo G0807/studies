a2bae90dd8b2c87c6e0eb81732c5fbafd93e238a
Guto <miltonaugusto56@gmail.com>
2025-02-17 18:42:16 -0300

    comparador de idade

---
+++ comparador_idade.py
@@ -0,0 +1,6 @@
+idade1 = int(input('Digite sua idade:'))
+idade2 = int(input('Digite a idade de outrs pessoa:'))
+if idade1 < idade2:
+    print ('Você é mais novo.')
+else:
+    print ('Você é mais velho.')    
