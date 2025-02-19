a392cd9b29ae04298545288baed43c05387ee2bc
Guto <miltonaugusto56@gmail.com>
2025-02-19 19:27:23 -0300

    questionario

---
+++ questao.py
@@ -0,0 +1,17 @@
+resposta = input('Qual é a capital do Brasil?\n a) São Paulo\n b) Rio de Janeiro\n c) Brasília\n d) Salvador\n').lower()
+if resposta == 'c':
+    print('Resposta correta!')
+else:
+    print('Resposta errada!')
+resposta = input('Qual é a capital da Argentina?\n a) Buenos Aires\n b) Santiago\n c) Lima\n d) Bogotá\n').lower()
+if resposta == 'a':
+    print('Resposta correta!')
+else:
+    print('Resposta errada!')
+resposta = input('Quanto é 20/4?\n a) 5\n b) 6\n c) 7\n d) 8\n').lower()
+if resposta == 'a':
+    print('Resposta correta!')
+else:
+     print('Resposta errada!')
+
+
