110f4fb0b11a23b10d7dbe4592fe7e1cb5d595ea
Guto <miltonaugusto56@gmail.com>
2025-02-19 20:06:06 -0300

    indentificador de letra

---
+++ novo.py
@@ -0,0 +1,8 @@
+letra = input('Digite uma letra: ').lower()
+if letra == 'a' or letra == 'e'or letra == 'i'or letra == 'o'or letra == 'u':
+    print('Letra digitada é uma vogal')
+else:
+    print('Letra digitada é uma consoante')
+
+
+ 
