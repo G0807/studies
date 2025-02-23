d1a5c0d3dd5545b969e519760145809bfb42c32e
Guto <miltonaugusto56@gmail.com>
2025-02-23 19:44:46 -0300

    usando a math. para sen,cos e tangente

---
+++ sen_cos_tan.py
@@ -0,0 +1,8 @@
+# seno, coseno e tangente de um ângulo
+import math
+angulo = float(input('Digite o valor do ângulo:'))
+seno = math.sin(math.radians(angulo))
+coseno = math.cos(math.radians(angulo))
+tangente = math.tan(math.radians(angulo))
+print(f'O seno do ângulo {angulo} é {seno:.2f}\n O cosseno é {coseno:.2f}\n \ \E a tangente é {tangente} ')
+#
