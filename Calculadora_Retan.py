73e64936e299c4c6f16137ca321481900b63a8b7
Guto <miltonaugusto56@gmail.com>
2025-02-23 09:22:58 -0300

    desconto de 5%

--- Calculadora_Retan.py
+++ Calculadora_Retan.py
@@ -1,6 +1,8 @@
 print('Calculadora de Retângulo')
-base = int(input('Digite a base do retângulo: '))
-altura = int(input('Digite a altura do retângulo: '))
+base = float(input('Digite a base do retângulo: '))
+altura = float(input('Digite a altura do retângulo: '))
 area = base * altura
-print('A área do retângulo é:{} '.format(area))
+print(f'A área do retângulo é:{area} ')
+litro = 2 * 1
+print(f'Uma lata de tinta pinta de 5 litros, pinta {area/litro:.2f} metros quadrados.')
 
---
+++ desconto.py
@@ -0,0 +1,5 @@
+valor = float(input('Digite o valor do produto: R$ '))
+desconto = valor * 0.05
+print(f'O valor do produto é R$ {valor:.2f} e com desconro de 5% é:{valor - desconto:.2f}R$ ')
+
+
