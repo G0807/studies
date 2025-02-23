c2293b92e34570f03091a2249c90b9aac204cb27
Guto <miltonaugusto56@gmail.com>
2025-02-23 15:13:16 -0300

    aumento de 15%

---
+++ aumnto.py
@@ -0,0 +1,5 @@
+salario = float(input('Digite o seu saláriol: '))
+aumento = salario * 0.085
+n_salario = salario + aumento
+print(f'O seu salário era de R${salario:.2f} e com o aumento de 8.5% passou a ser R${n_salario:.2f}')
+
