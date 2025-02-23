81df0b297b28d4adfafb9ca1fbd426a8c51386bd
Guto <miltonaugusto56@gmail.com>
2025-02-23 17:05:27 -0300

    aula de modulos

---
+++ modulos2.py
@@ -0,0 +1,12 @@
+import random
+num = random.randint(1, 100)
+print(f'O número sorteado foi {num}')
+#
+import emoji
+print(emoji.emojize('Python is :thumbs_up:'))
+print(emoji.emojize('😮'))
+
+      
+        
+
+
