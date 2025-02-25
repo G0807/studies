38e733752d2f6abb5e1dac77db15ccb727bddd36
Guto <miltonaugusto56@gmail.com>
2025-02-25 20:00:20 -0300

    fatiameno e manipulação de str

---
+++ fatiamento.py
@@ -0,0 +1,22 @@
+frase = 'Curso em Vídeo Python'
+print(frase[9])
+print(frase[9:13])
+print(frase[9:21])
+print(frase[9:21:2])
+print(frase[:5])
+print(frase[15:])
+print(frase[9::3])
+print(frase[::2])
+print(frase.upper().count('o'))
+print(len(frase))
+print(frase.replace('Python', 'Android'))
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.split())
+frase = '  Aprenda python '
+print(frase.strip())
+print(frase.lstrip())
+print(frase.rstrip())
+
+
+
