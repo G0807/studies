7789711a047a16ebb18b785ad48f47b34d5978b7
Guto <miltonaugusto56@gmail.com>
2025-02-26 19:51:49 -0300

    fatiamento de string

---
+++ aula_cidade.py
@@ -0,0 +1,13 @@
+cit = input('Digite o nome de uma cidade:').lower()
+print('santo' in cit)
+#fim
+frase = input('Digite uma frase:').lower()
+
+print(f'Há {frase.count("a")} letras "a" na frase')
+print(f'A primeira letra "a" aparece na posição {frase.find("a")+1}')
+print(f'A última letra "a" aparece na posição {frase.rfind("a")+1}')
+#fim
+nome =input('Digite seu nome completo:').strip()
+print(f'Seu nome em maiúsculas é: {nome.upper()}\nSeu nome em minúsculas é: {nome.lower()})')
+print(f'Seu primeiro nome tem {len(nome.split()[0])} letras')
+#fim
