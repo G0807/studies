d488eb45d02f31f607d3ce30e5946efe957ae466
Guto <miltonaugusto56@gmail.com>
2025-02-26 18:32:17 -0300

    Fatiamento de string

---
+++ aula_nome.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome completo: ')
+lista = nome.split()
+print(f'Seu primeiro nome é: {lista[0]}\nSeu último nome é: {lista[-1]}')
+#fim
+number = input('Digite um número de 4 digitos:')
+print(f'Milhar: {number[0]}\nCentena: {number[1]}\nDezena: {number[2]}\nUnidade: {number[3]}')
+#fim
