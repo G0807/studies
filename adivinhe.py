39bed858a14bea2b7a882e148de1ae880cb7cabb
Guto <miltonaugusto56@gmail.com>
2025-03-04 15:31:25 -0300

    usando condicionais e loop

---
+++ adivinhe.py
@@ -0,0 +1,13 @@
+import random
+print('Adivinhe o número de 0 a 5. Qual número estou pensando? ')
+numero  = [1, 2, 3, 4, 5]
+sorte = random.choice(numero)
+while True:
+    resposta = int(input( 'Digite o número que estou pensendo:'))
+    if resposta == sorte:
+        print('Você acertou!')
+        break
+    else:
+        print('Você errou! Tente novamente.')
+        #fim
+
