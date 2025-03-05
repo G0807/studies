3ced82eddc05e801b662cba8c90e3cf85be170bc
Guto <miltonaugusto56@gmail.com>
2025-03-05 14:09:19 -0300

    cond.aninhadas

---
+++ condicionaisp2.py
@@ -0,0 +1,11 @@
+nome = input('Digite seu nome:').capitalize()
+if nome == 'Milton':
+   print(f'Bom dia, {nome.capitalize()}! Que nome bonito! ')
+elif nome == 'João' or nome == 'Pedro' or nome == 'Tiago':
+   print(f'Bom dia, {nome.capitalize()}! O seu nome é Biblíco!')
+else:
+   print(f'Bom dia, {nome.capitalize()}!')
+
+
+
+
