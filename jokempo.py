5ce57eb276db4947be77fc90b2c875d9d00f1a08
Guto <miltonaugusto56@gmail.com>
2025-03-06 18:52:41 -0300

    condicionais

---
+++ jokempo.py
@@ -0,0 +1,30 @@
+import random
+resposta = input('Vamos jogar Jokem-Po? ').lower().strip()
+if resposta == 'sim':
+    mao = input('O timo! Já escolhi minha mão! Digite a sua! pedra, papel ou tesoura?')
+mao_pc = ['pedra', 'papel', 'tesoura']
+sorte = random.choice(mao_pc)
+if  sorte == 'pedra' and mao == 'pedra':
+    print('Empatamos!')
+elif sorte == 'pedra' and mao == 'papel':
+    print('Você venceu!')
+elif sorte == 'pedra' and mao == 'tesoura':
+    print('Você perdeu!')
+elif sorte == 'papel' and mao == 'pedra':
+        print('Você perdeu!')
+elif sorte == 'papel' and mao == 'tesoura':
+        print('Você venceu!')
+elif sorte == 'papel' and mao == 'papel':
+        print('Empatamos!')   
+elif sorte == 'tesoura' and mao == 'pedra':
+        print('Você venceu!')
+elif sorte == 'tesoura' and mao == 'tesoura':
+        print('Empatamos!')
+elif sorte == 'tesoura' and mao == 'papel':
+        print('Você venceu!')
+else:
+    print('Fica para próxima!')
+
+
+
+
