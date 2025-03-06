33c27b7e2196e310cfab391ddc4b2e26d7f646c8
Guto <miltonaugusto56@gmail.com>
2025-03-06 19:21:18 -0300

    condicionais e loop

--- cadastroMeu.py
+++ cadastroMeu.py
@@ -5,12 +5,12 @@ def cadastrar_pessoas():
         idade = int(input('Digite a sua idade:'))
         sexo = input('Digite o seu sexo:')
         natural = input('Digite a sua nacionalidade:')
-        confirm = input('Os dados estão corretos? Digite ok para confirmar')
+        confirm = input('Os dados estão corretos? Digite ok para confirmar ')
         if confirm.lower() == 'ok':
             print('Cadastro realizado com sucesso!')
             break
         else:
             print('Falha no cadastro, tente novamente!')
 cadastrar_pessoas()
-            
+
     
