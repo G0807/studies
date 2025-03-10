1777ea02a72d8e43fe9ad4b701d8c96113c5e620
Guto <miltonaugusto56@gmail.com>
2025-03-10 18:02:48 -0300

    primos

---
+++ numprimos.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um numero:'))
+if num / num == 1 and num / 1 == num and num != 1:
+    print(f'O número {num} é um número primo.')
+else:
+    print(f'O número {num} não é primo.')     
