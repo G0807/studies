0cb7a04b8681a4a7bdc614c3e969ca6888e8760d
Guto <miltonaugusto56@gmail.com>
2025-03-12 05:15:01 -0300

    laço

--- palinromo.py
+++ palinromo.py
@@ -3,11 +3,12 @@ palavras = frase.split()
 junto = ''.join(palavras)
 inverso = ''
 inverso = junto[::-1]
-#for letra in range(len(junto)-1,-1,-1):
-    #inverso += junto[letra]
+#usando "for"para resolver de un jeito diferente
+'''for letra in range(len(junto)-1,-1,-1):
+    inverso += junto[letra]'''
 print(f'O inverso de {junto} é {inverso}')
 if junto == inverso:
-    print('Temos um Palpindromo!')
+    print('Temos um Palíndromo!')
 else:        
     print('A frase digitada não é um palíndromo!')
 
